ab9090cb95306d3fde21a7a7f9378fd8a9169fe7
Benjamin Delmée <benjamin.delmee@gmail.com>
2019-10-18 21:44:22 +0200

    add filter to scrapp only the right announcements

--- scripts/get_announcements.py
+++ scripts/get_announcements.py
@@ -1,17 +1,26 @@
+from urllib.parse import urljoin
 import requests
 from bs4 import BeautifulSoup
-from scrapping import workflow, download, parse, extract_table
+from scrapping import step, workflow, download, parse, extract_table
 
 @step
 def extract_urls(soup):
-    # TODO
-    # new_links = soup.find('section', {'id': 'page-content'}) \
-    #     .find('div', {'class': 'view-content'}) \
-    #     .find_all('a')
-    # return set([urljoin('http://www.tesoro.es', x.attrs['href']) for x in new_links])
+    new_links = soup.find('section', {'id': 'page-content'}) \
+        .find('div', {'class': 'view-content'}) \
+        .find_all('a')
+    
+    texts = [x.text for x in new_links]
+    urls = [urljoin('http://www.tesoro.es', x.attrs['href']) for x in new_links]
+ 
+    def keep(text):
+        if 'announces' in text:
+            return True
+        return False
+
+    return set([url for text, url in zip(texts, urls) if keep(text)])
 
 @workflow
-def get_calendar_treasury_letters():
+def get_announcements():
     url = 'http://www.tesoro.es/en/deuda-publica'
     html = download(url)
     soup = parse(html)
@@ -27,8 +36,7 @@ def get_calendar_treasury_letters():
     return datas
 
 if __name__ == '__main__':
-    # err, data = get_calendar_bonds_and_obligations()
-    # err, data = get_calendar_treasury_letters()
+    # err, data = get_announcements()
     # if err:    
     #     print(err)
     # else:
